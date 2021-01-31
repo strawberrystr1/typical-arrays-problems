c9f94aaf18f205b8a0e62d020165fb4bd5d54219
strawberrystr1 <andrey_overdose@mail.ru>
2021-01-31 20:14:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!arguments.length || array.length === 0) {
+    return 0;
+  } else {
+    array.sort((a,b) => a-b);
+    return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!arguments.length || array.length === 0) {
+    return 0;
+  } else {
+    array.sort((a,b) => b-a);
+    return array[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!arguments.length || array.length === 0) {
+    return 0;
+  } else {
+    return (array.reduce((sum, current) => sum + current, 0))/array.length;
+  }
 }
+
